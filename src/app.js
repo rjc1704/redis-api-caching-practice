170dbf6dfed17d772919174092899c7562c07fb7
Jecheon Ryu <rjc1704@gmail.com>
2025-06-24 01:28:31 +0900

    multer-s3 적용하여 버킷 public 폴더 업로드

--- src/app.js
+++ src/app.js
@@ -1,32 +1,49 @@
-import express from 'express';
+import express from "express";
 import { PrismaClient } from "@prisma/client";
-import multer from 'multer';
+import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
 
 const app = express();
 const prisma = new PrismaClient();
 
-app.use('/photos', express.static('photos'));
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
-const upload = multer({ dest: 'photos/' });
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    key: (req, file, cb) => {
+      cb(null, `public/${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
 
-app.route('/')
+app
+  .route("/")
   .get(async (req, res) => {
     const diaryEntries = await prisma.diaryEntry.findMany();
     return res.status(200).json(diaryEntries);
   })
-  .post(upload.single('photo'), async (req, res) => {
+  .post(upload.single("photo"), async (req, res) => {
     const { date, content } = req.body;
-    const { path } = req.file;
-    const diaryEntry = await prisma.diaryEntry.create({
+    const { location } = req.file;
+    const diary = await prisma.diaryEntry.create({
       data: {
         date: new Date(date),
         content,
-        photoUrl: path,
+        photoUrl: location,
       },
     });
-    return res.json(diaryEntry);
+    res.json(req.file);
   });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
